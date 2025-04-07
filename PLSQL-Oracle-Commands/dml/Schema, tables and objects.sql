54cef9222b4ae992d4f356f0ecd9f6053954f63c
Fabricio Leite <fm.almeida.leite@gmail.com>
2025-04-07 14:05:55 -0300

    Includes PLSQL command

--- PLSQL-Oracle-Commands/dml/Schema, tables and objects.sql
+++ PLSQL-Oracle-Commands/dml/Schema, tables and objects.sql
@@ -1,4 +1,25 @@
---Search for columns throughout the schema
+---------------------------
+-- View columns in a table 
+---------------------------
+SELECT COL.COLUMN_ID, 
+       COL.OWNER AS SCHEMA_NAME,
+       COL.TABLE_NAME, 
+       COL.COLUMN_NAME, 
+       COL.DATA_TYPE, 
+       COL.DATA_LENGTH, 
+       COL.DATA_PRECISION, 
+       COL.DATA_SCALE, 
+       COL.NULLABLE
+FROM SYS.ALL_TAB_COLUMNS COL
+INNER JOIN SYS.ALL_TABLES T ON COL.OWNER = T.OWNER 
+	AND COL.TABLE_NAME = T.TABLE_NAME
+WHERE 
+	COL.TABLE_NAME = 'PRODUCT' --case sensitive
+ORDER BY COL.COLUMN_ID;
+
+--------------------------------------------
+-- Search for columns throughout the schema
+--------------------------------------------
 SELECT COL.COLUMN_ID, 
        COL.OWNER AS SCHEMA_NAME,
        COL.TABLE_NAME, 
@@ -15,7 +36,9 @@ WHERE
 	COL.COLUMN_NAME LIKE  '%PRODUCT%' --case sensitive
 ORDER BY COL.COLUMN_ID;
 
---View tables that reference a particular table
+----------------------------------------------
+-- Search tables that reference a given table
+----------------------------------------------
 SELECT 
     a.CONSTRAINT_NAME,
     a.TABLE_NAME,
@@ -28,3 +51,25 @@ JOIN
 WHERE 
     a.CONSTRAINT_TYPE = 'R' --foreign key
     AND b.TABLE_NAME = 'PRODUCT'; --case sensitive
+	
+---------------------------------------------
+-- Search for foreign keys for a given table
+---------------------------------------------
+SELECT
+    A.CONSTRAINT_NAME,
+    A.TABLE_NAME,
+    C.COLUMN_NAME,
+    A.R_CONSTRAINT_NAME,
+    B.TABLE_NAME AS REFERENCED_TABLE_NAME,
+    D.COLUMN_NAME AS REFERENCED_COLUMN_NAME
+FROM
+    ALL_CONSTRAINTS A
+JOIN
+    ALL_CONS_COLUMNS C ON A.CONSTRAINT_NAME = C.CONSTRAINT_NAME
+JOIN
+    ALL_CONSTRAINTS B ON A.R_CONSTRAINT_NAME = B.CONSTRAINT_NAME
+JOIN
+    ALL_CONS_COLUMNS D ON B.CONSTRAINT_NAME = D.CONSTRAINT_NAME AND C.POSITION = D.POSITION
+WHERE
+    A.CONSTRAINT_TYPE = 'R'
+    AND A.TABLE_NAME = 'PRODUCT'; --case sensitive
